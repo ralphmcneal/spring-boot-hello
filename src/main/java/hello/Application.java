ec409b436d9811b7762390ff4f08880ae49d6599
ralphmcneal <ralph.mcneal@centurylink.com>
2017-03-23 13:40:05 -0500

    change text

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	    @RequestMapping("/")
 		        public String home() {
-				        return "Hello Kubernetes!!";
+				        return "Hello Kubernetes from Spring Boot!!";
 					    }
 
 	        public static void main(String[] args) {
